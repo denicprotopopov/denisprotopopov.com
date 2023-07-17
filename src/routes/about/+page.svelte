1cf57d96836e9e2eab23ad0601330549692866b8
Denis Protopopov <protdenden@gmail.com>
2023-07-17 12:48:04 +0400

    Updated bio

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -19,37 +19,243 @@
                     </div>
                 </div>
             </div>
+            <div class="row">
+                <a class="cell" href="../">
+                    <div class="with-arrow">
+                        <h3>To main page</h3>
+                        <div class="arrow"></div>
+                    </div>
+                </a>
+
+                <a class="cell" href="mailto:protdenden@gmail.com">
+                    <div class="with-arrow">
+                        <h3>Contact me</h3>
+                        <div class="arrow"></div>
+                    </div>
+            </div>
+
+            <div class="row">
+                <div class="cell">
+                <img class="image" src="../images/me.png">
+            </div>
+        </div>
           
             <div class="row">
                 <div class="cell">
-                    <h3>Bio</h3>
-                    <p>Denis Protopopov works at the intersection of research, education, technology, media, visual and performing arts.</p>
-
-                        <p>Denis acquired his Education from Bard College (US) and Smolny College (RU).</p>
-                        
-                            <p>He participated in Ars Electronica (AT), and a number of museum shows, including Hermitage, VDNKh, AIR gallery, Electromuseum, Central Museum of Communications.</p>
-                        
-                                <p>His performances were staged on Meyerhold Center, Kuryokhin Center, Alexandrinsky Theatre and Kuryokhin Center.</p>
-                        
-                                    <p>Denis lectured and leg seminars and workshops in Strelka Institute, New Media Lab (RU), Meyerhold Center, Goethe Institut and others.</p>
-                        
-                                        <p>He developed online and offline educational projects for Practicum Bootcamp, Meyerhold Center, PERMM, Strelka Institute and others.</p>
+                    <p>I am a creative developer and STEM learning experience designer driven by a passion for education, art, and making technology transparent. Over the past five years, I have developed more than 10 artistic projects that have utilized various cutting-edge technologies, such as AI, VR, and AR. I have held LX-designer positions in two edTech companies that are among the top three leaders on the CIS market (Yandex Practicum and SkillFactory).</p>
                 </div>
             </div>
-
-
 
             <div class="row">
                 <div class="cell">
-                    <h3>On practice</h3>
-                    <p>My art, research and educational interests span across algorithmic biases, computation and sexuality, technological embodiment, machine magic and government’s use of machine learning for political repression. In art practice these topics are often expressed, using video, sound, text-based art, augmented reality, interactive fiction, theatre and performance.</p>
-
-                        <p>Critical dataset studies, data subjectivity, algorithmic biases, natural language processing and computer vision, seen through the prism of queer, precarious labour that shapes complex systems – all these and many other topics open the ‘black box’, and reveal processes, limitations, and the danger that acceleration introduces by shaping our surrounding world.</p>
-
-                            <p>Education and learning are major parts of my work. I believe that thee growing importance of software requires a more broad way of curriculum that will help to better understand the politics and aesthetics of data processing, algorithms, and complex systems. It is important to question innovation narratives, environmental aspects, accessibility, binary logic, hierarchies and biases. That is why I design and develop STEM educational projects for artists, performers and underrepresented minority groups.</p>
+                   <p>With a background in computer science and visual arts, I have gained experience in programming and design. My work has been exhibited in various notable venues such as Ars Electronica and the Hermitage, in the form of websites, mobile apps, and games. This experience helped me develop skills that now enable me to create technologically advanced yet accessible and engaging projects.</p>
                 </div>
             </div>
 
+            <div class="row">
+                <div class="cell">
+                   <p>Education is a field where I see an opportunity for actual change towards a more just and equal world. This is why, in addition to working with edTech companies, I have produced and developed two independent educational projects for artists and activists. My work in this area has been recognized, including a mention in <a class="intext" href="https://www.forbes.ru/finansy/492731-rosstat-soobsil-ob-uskorenii-godovoj-inflacii-v-iune-do-3-25">Forbes</a> for developing the first educational project on digital art in Russia.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="cell">
+                   <h2bright>Experience</h2bright>
+                </div>
+            </div>
+
+            <div class="row">
+                <a class="cell" href="../art">
+                    <div class="with-arrow">
+                        <h3>To projects</h3>
+                        <div class="arrow"></div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="row">
+                <div class="cell">
+                    <h3>Freelance Artist and Developer</h3>
+                    <p><b>2017 — now</b></p>
+                    <p>Back in college, I utilized my development skills to create artistic projects and theater performances. I have developed websites, mobile apps, and games using JavaScript, Svelte, Three.js, Swift, and Unity.</p>
+                    <p>I have also worked with video and animation, including video art and multimedia theatrical scenery made using Cinema 4D, Apple Motion, Final Cut Pro, and mapping with Resolume Arena.</p>
+                    <p>I have participated in more than 10 group and 4 solo exhibitions, including performances, in Russia and internationally. As an artist, I have been invited to multiple venues to hold lectures and artist talks, including Goethe Institut, HSE Art and Design School, and Strelka Institute.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="cell">
+                    <h3>Yandex Practicum</h3>
+                    <p><b>2021 — now</b></p>
+                    <p><b>Learning Experience Designer</b></p>
+                    <p>I joined a team where I was responsible for the learning experience of five web development products. During my time here, I developed and implemented a pipeline that utilizes student behavior data to execute metric-based content refactoring, which helped to maintain a completion rate of over 50% and an employability rate of over 70%.</p>
+                    <p>I also redesigned the curriculum and learning experience of two online reskilling bootcamps: "Frontend Developer" and "Fullstack Developer". Currently, I consult with the Practicum team on the learning experience of the "Soft Skills for Developers" course.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="cell">
+                    <h3>Meyerhold Center</h3>
+                    <p><b>2020 — 2022</b></p>
+                    <p><b>Resident</b></p>
+                    <p>After winning the Blackbox residency, I became a permanent resident of the Meyerhold Center, one of the most important theatrical and artistic platforms in Russia. Here, I conceptualized and directed the algorithmic theater performance "Rockets Take Off and Crash and Scatter In the Air," which successfully ran for two theatrical seasons.</p>
+                    <p>During my time at the Meyerhold Center, I produced and developed the "Digital Theater Workshop," the first online course on digital arts in Russia.</p>
+                    <p>I was no longer a resident of the Meyerhold Center when the theater was closed by the Russian government due to the anti-war position of its director.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="cell">
+                    <h3>SkillFactory</h3>
+                    <p><b>2020 — 2021</b></p>
+                    <p><b>Learning Experience Designer for Higher Education</b></p>
+                    <p>I joined the product team as its first member to work on a non-existent product - a service that would help established universities create online graduate programs in Data Science and Machine Learning.</p>
+                    <p>I worked on creating the entire educational experience, production pipeline, core values, and principles. Together with the team, we launched the first-ever online master's program in Data Science in Russia. Besides LX-design work, I also conceptualized, designed, and coded explorable explanations and wrote the entire "Basic Linux" course as an expert.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="cell">
+                   <h2bright>Educational Career</h2bright>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="cell">
+                    <h3>Politics of Culture. Fundraising festival in aid of Ukrainian refugees</h3>
+                    <p><b>2022</b></p>
+                    <p><b>Lecturer</b></p>
+                    <p>I did a lecture dedicated to social prejudices in AI systems and its connections to modern wars.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="cell">
+                    <h3>HSE Art and Design School</h3>
+                    <p><b>2022</b></p>
+                    <p><b>Visiting Lecturer</b></p>
+                    <p>There was a lecture and a seminar dedicated to machine magic and electronic music in early 90s.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="cell">
+                    <h3>ai4a. Artificial Intelligence: Principles, Art and Politics</h3>
+                    <p><b>2021</b></p>
+                    <p><b>Author, producer, developer</b></p>
+                    <p>My personal educational project. It was an online lab for artists, designers, and activists, dedicated to various aspects of AI, including basic principles, biases, and social impact. I designed the course, wrote theoretical modules, and coded Jupyter Notebooks that allowed people who have never coded before to explore various ML algorithms and use them in their projects</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="cell">
+                    <h3>Strelka Institute of Architecture, Media and Design</h3>
+                    <p><b>2020</b></p>
+                    <p><b>Visiting Lecturer</b></p>
+                    <p>Micro-course, dedicated to the collection of a speculative dataset.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="cell">
+                    <h3>Masters Art School</h3>
+                    <p><b>2019</b></p>
+                    <p><b>Lecturer</b></p>
+                    <p>I made a series of lectures on the history of media art.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="cell">
+                    <h3>New Media Lab (Russia)</h3>
+                    <p><b>2019</b></p>
+                    <p><b>Lecturer</b></p>
+                    <p>I held a micro-course on the use of AI in Theater Production.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="cell">
+                   <h2bright>Writings</h2bright>
+                </div>
+            </div>
+
+            <div class="row">
+                <a class="cell" href="https://bbbl.dev/articles/low-tech">
+                    <div class="with-arrow">
+                        <h3>What is Low-Tech Web Community and How Will It Help WIn The War Against Platforms, Bubble, 2022</h3>
+                        <div class="arrow"></div>
+                    </div>
+                </a>
+            </div>
+    
+            <div class="row">
+                <a class="cell" href="https://supernova.is/editorial/ai-bias">
+                    <div class="with-arrow">
+                        <h3>Why AI Copies Our Biases and How To Fix It, Supernova, 2022</h3>
+                        <div class="arrow"></div>
+                    </div>
+                </a>
+            </div>
+    
+            <div class="row">
+                <a class="cell" href="https://nonfiction.ru/stream/algoritm-sluchajnoj-realnosti-mashinnyij-intellekt-v-iskusstve">
+                    <div class="with-arrow">
+                        <h3>Random Reality Algorithm: Machine Intelligence in Art, Alpina Non-Fiction, 2021</h3>
+                        <div class="arrow"></div>
+                    </div>
+                </a>
+            </div>
+    
+            <div class="row">
+                <a class="cell" href="https://cognitive.sovremennik.ru/znanie/14-instrumenty-tsifrovogo-iskusstva">
+                    <div class="with-arrow">
+                        <h3>Instruments of Digital Art, Cognitive, 2021</h3>
+                        <div class="arrow"></div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="row">
+                <div class="cell">
+                   <h2bright>Publicity</h2bright>
+                </div>
+            </div>
+
+            <div class="row">
+                <a class="cell" href="http://artuzel.com/content/rakety-vzletayut-i-rassypayutsya-v-vozduhe-kak-sozdat-spektakl-iz-tryoh-pes-na-osnove">
+                    <div class="with-arrow">
+                        <h3>How To Create A Performance Of Three Plays Based On a Computer Algorithm, Artuzel, 2021</h3>
+                        <div class="arrow"></div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="row">
+                <a class="cell" href="https://syg.ma/@eugenia-ouliankina/rakiety-i-konfiety-ghid-po-spiektakliu-o-sviazi-vsiegho-so-vsiem">
+                    <div class="with-arrow">
+                        <h3>Rockets and Candies: a Guide to the Play About the Connection of Everything With Everything, syg.ma, 2021</h3>
+                        <div class="arrow"></div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="row">
+                <a class="cell" href="http://oteatre.info/hod-veshhej-dva-spektaklya-festivalya-noname/">
+                    <div class="with-arrow">
+                        <h3>The Course of Things: Two Performances of the Noname Festival, Theater Magazine, 2020</h3>
+                        <div class="arrow"></div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="row">
+                <a class="cell" href="https://obdn.ru/articles/denis-protopopov">
+                    <div class="with-arrow">
+                        <h3>The Course of Things: Two Performances of the Noname Festival, Theater Magazine, 2020</h3>
+                        <div class="arrow"></div>
+                    </div>
+                </a>
+            </div>
             
             
     </div>
@@ -162,9 +368,9 @@
         }
 
         p, ol, li {
-            font-size: clamp(10px, 4vw, 36px);
-            color: #949494;
-            margin: 8px 0 0;
+            font-size: clamp(10px, 4vw, 24px);
+            color: #b4b4b4;
+            margin: 2px 0 0;
         }
 
         a {
@@ -172,8 +378,13 @@
             text-decoration: none;
         }
 
+        .intext {
+            color: currentColor;
+            text-decoration: underline;
+        }
+
         .link {
-            color: #594bc4;
+            color: #524b8e;
         }
 
         @media (hover: hover) and (pointer: fine) {
